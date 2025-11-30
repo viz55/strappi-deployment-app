c4eeff8d97e1ab95b82dbbe0a4ec1d73db455f73
Its me Vishnu M V <vishnumv550@gmail.com>
2025-11-30 16:03:37 +0530

    Delete terraform directory

--- terraform/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "ec2_public_ip" {
-  description = "Public IP of the EC2 instance"
-  value       = aws_instance.strapi.public_ip
-}
-
-output "rds_endpoint" {
-  description = "RDS endpoint address"
-  value       = aws_db_instance.strapi.address
-}
