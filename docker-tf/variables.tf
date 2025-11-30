586a778e4f7d2d09856a23543ec4560c36c9ba4d
Its me Vishnu M V <vishnumv550@gmail.com>
2025-11-30 17:48:20 +0530

    Update variables.tf

--- docker-tf/variables.tf
+++ docker-tf/variables.tf
@@ -1,5 +1,5 @@
 
-variable "region" { default = "us-east-1" }
+variable "region" { default = "ap-south-1" }
 variable "ami" { description = "EC2 AMI" }
 variable "docker_image" { description = "Docker image to run" }
 variable "db_name" {}
