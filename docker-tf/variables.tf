fc2eaad5f59bf8ae7cf289b28b77d80911ee7ad3
Its me Vishnu M V <vishnumv550@gmail.com>
2025-12-01 02:58:04 +0530

    Update variables.tf

--- docker-tf/variables.tf
+++ docker-tf/variables.tf
@@ -1,7 +1,6 @@
-
 variable "aws_region" { default = "ap-south-1" }
 variable "ami" { description = "EC2 AMI" }
-variable "docker_image" { description = "Docker image to run" }
+variable "docker_image" { default = "viz55/strapi-app:latest" }
 variable "db_name" {}
 variable "db_user" {}
 variable "db_password" {}
