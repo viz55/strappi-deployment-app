4d6db058e1408822ecc1d9b815f7e8836027f967
Its me Vishnu M V <vishnumv550@gmail.com>
2025-11-30 18:28:42 +0530

    Update variables.tf

--- docker-tf/variables.tf
+++ docker-tf/variables.tf
@@ -1,5 +1,5 @@
 
-variable "region" { default = "ap-south-1" }
+variable "aws_region" { default = "ap-south-1" }
 variable "ami" { description = "EC2 AMI" }
 variable "docker_image" { description = "Docker image to run" }
 variable "db_name" {}
